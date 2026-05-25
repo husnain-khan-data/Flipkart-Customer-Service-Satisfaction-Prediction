95eccedec2d69a24e62f1784485e1a74110498d4
Husnain Khan <khanhusnain78@gmail.com>
2026-05-25 14:24:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import joblib
 import numpy as np
+import pandas as pd
+import math
 
 # Load model
-model = joblib.load("small_rf_model.pkl")
+model = joblib.load('small_rf_model.pkl')
 
 # Title
 st.title("Flipkart Customer Satisfaction Prediction")
@@ -11,29 +13,60 @@ st.title("Flipkart Customer Satisfaction Prediction")
 st.write("Enter customer details to predict CSAT Score")
 
 # Inputs
-agent_experience = st.number_input("Agent Experience Level", min_value=0)
 
-response_time = st.number_input("Response Time", min_value=0)
+channel_name = st.selectbox(
+    "Channel Name",
+    [0, 1, 2]
+)
 
-issue_resolved = st.selectbox("Issue Resolved", [0, 1])
+category = st.selectbox(
+    "Category",
+    [0, 1, 2, 3]
+)
 
-customer_rating = st.slider("Customer Rating", 1, 5)
+item_price = st.number_input(
+    "Item Price",
+    min_value=0.0
+)
 
-# Dummy extra features
-feature5 = 0
-feature6 = 0
-feature7 = 0
+connected_handling_time = st.number_input(
+    "Connected Handling Time",
+    min_value=0.0
+)
+
+agent_shift = st.selectbox(
+    "Agent Shift",
+    [0, 1, 2]
+)
+
+response_time = st.number_input(
+    "Response Time",
+    min_value=1.0
+)
+
+response_time_log = math.log(response_time)
 
 # Prediction
+
 if st.button("Predict CSAT Score"):
 
-    input_data = np.array([[agent_experience,
-                            response_time,
-                            issue_resolved,
-                            customer_rating,
-                            feature5,
-                            feature6,
-                            feature7]])
+    input_data = pd.DataFrame([[
+        channel_name,
+        category,
+        item_price,
+        connected_handling_time,
+        agent_shift,
+        response_time,
+        response_time_log
+    ]], columns=[
+        'channel_name',
+        'category',
+        'Item_price',
+        'connected_handling_time',
+        'Agent Shift',
+        'Response_Time',
+        'Response_Time_Log'
+    ])
 
     prediction = model.predict(input_data)
 
